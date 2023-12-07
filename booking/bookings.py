4ccd7ac467d27c0e980c2d46f16917d5134aa69c
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-12-07 12:23:37 +0400

    Add functionality to quit the bot

--- booking/bookings.py
+++ booking/bookings.py
@@ -1,13 +1,22 @@
+import types
+import typing
 from selenium import webdriver
 import os
 import booking.constants as const
 
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r"/usr/local/bin/chromedriver"):
+    def __init__(self, driver_path=r"/usr/local/bin/chromedriver", teardown = False):
         self.driver_path = driver_path
+        self.teardown = teardown
         # os.environ['PATH'] += self.driver_path
         super(Booking, self).__init__()
+        self.implicitly_wait(15)
 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if(self.teardown):
+            self.quit()
 
     def land_first_page(self):
         self.get(const.BASE_URL)
+
+   
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from booking.bookings import Booking
 
-inst = Booking()
-inst.land_first_page()
+with Booking() as bot: 
+    bot.land_first_page()
+    print('Exiting...')
